b5f92533ddbdba2da5051137ec4dbc78da4bbf0b
orjon <github@orjon.com>
2020-08-21 21:43:51 +0100

    Fixed double hello appearing on touch device click

--- scripts/intro.js
+++ scripts/intro.js
@@ -2,10 +2,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const oCircle = document.querySelector('#circleWhite')
   const helloText = document.querySelector('#hello')
+  const helloTouchText = document.querySelector('#helloTouch')
 
 
   oCircle.addEventListener('mouseover', () => {
     helloText.innerHTML='hello'
+    helloTouchText.innerHTML=''
   })
 
   oCircle.addEventListener('mouseout', () => {
