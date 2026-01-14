f25b4ba270fa0bd1d5a1f339bc9c86c199717470
orjon <github@orjon.com>
2026-01-14 18:10:02 +0000

    basic retro menu skeuomorphism

---
+++ app/electronics/page.tsx
@@ -0,0 +1,8 @@
+const ElectronicsPage = () => {
+  return (
+    <div>
+      <h1>Electronics Page</h1>
+    </div>
+  )
+}
+export default ElectronicsPage
--- components/nav/Menu.tsx
+++ components/nav/Menu.tsx
@@ -1,17 +1,47 @@
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 const Menu = () => {
+  const pathname = usePathname()
   return (
-    <div className='hidden md:flex px-2  flex-row gap-4 bg-grey100'>
-      <Link href='/code'>
-        <h1 className='text-lg font-bold'>code</h1>
-      </Link>
-      <Link href='/design'>
-        <h1 className='text-lg font-bold'>design</h1>
-      </Link>
-      <Link href='/visualization'>
-        <h1 className='text-lg font-bold'>visualization</h1>
-      </Link>
+    <div className='hidden md:flex flex-row bg-grey300 text-grey050 text-lg font-medium'>
+      <div className='group w-40 splitGrey duration-100'>
+        <Link
+          href='/code'
+          className={`block w-full p-1 bg-grey300 mb-2 group-hover:mb-0 group-hover:mt-1 duration-100 ${
+            pathname === '/code' ? 'text-code' : 'text-grey050'
+          } shadow-[inset_0px_0px_1px_0px_var(--color-grey400)]`}
+        >
+          code
+        </Link>
+      </div>
+
+      <div className='group w-40 splitGrey  duration-100'>
+        <Link
+          href='/design'
+          className={`block w-full p-1 bg-grey300 mb-2 group-hover:mb-0 group-hover:mt-1 duration-100 ${
+            pathname === '/design' ? 'text-design' : 'text-grey050'
+          } shadow-[inset_0px_0px_1px_0px_var(--color-grey400)]`}
+        >
+          design
+        </Link>
+      </div>
+
+      <div className='group w-40 splitGrey duration-100'>
+        <Link
+          href='/electronics'
+          className={`block w-full p-1 bg-grey300 mb-2 group-hover:mb-0 group-hover:mt-1 duration-100 ${
+            pathname === '/electronics' ? 'text-electronics' : 'text-grey050'
+          } shadow-[inset_0px_0px_1px_0px_var(--color-grey400)]`}
+        >
+          electronics
+        </Link>
+      </div>
+      <div className='group w-full splitGrey  duration-100'>
+        <div className='block w-full p-1 bg-grey300 mb-2 text-grey300'>
+          nothing to see here...
+        </div>
+      </div>
     </div>
   )
 }
